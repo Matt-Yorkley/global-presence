e95b51f24f3d13677454d6a15b6405ad8e36838a
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-01 17:51:48 +0000

    Add Stimulus Vite helpers

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -13,16 +13,4 @@ console.log('Vite ⚡️ Rails')
 
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+import "../controllers"
