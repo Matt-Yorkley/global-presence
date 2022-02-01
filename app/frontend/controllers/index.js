db73d849bd9719859741dbff3dcc19579fb9b9b7
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-01 17:55:57 +0000

    CR and CR setup / install tasks

--- app/frontend/controllers/index.js
+++ app/frontend/controllers/index.js
@@ -3,7 +3,13 @@
 
 import { Application } from "stimulus"
 import { registerControllers } from 'stimulus-vite-helpers'
+import StimulusReflex from 'stimulus_reflex'
+import consumer from '../channels/consumer'
+import controller from '../controllers/application_controller'
 
 const application = Application.start()
 const controllers = import.meta.globEager('./**/*_controller.js')
 registerControllers(application, controllers)
+application.consumer = consumer
+StimulusReflex.initialize(application, { controller, isolate: true })
+StimulusReflex.debug = import.meta.env.DEV
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_controller.default_url_options = {host: "localhost", port: 3000}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
