ed452a78d612bbc8a908a5ea5b02e834a0fb5fcc
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-01 18:54:06 +0000

    Set up ActionCable

---
+++ app/frontend/channels/index.js
@@ -0,0 +1,4 @@
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+
+const channels = import.meta.globEager('./**/*_channel.js');
--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,16 +1,5 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
 console.log('Vite ⚡️ Rails')
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
 import "../controllers"
+import "../channels"
+
