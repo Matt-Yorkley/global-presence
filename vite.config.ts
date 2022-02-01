29801ba8f051b0959509637dde22e3b0f0c3f31a
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-01 18:19:52 +0000

    Add Vite plugins for better dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import FullReload from 'vite-plugin-full-reload'
 import RubyPlugin from 'vite-plugin-ruby'
+import StimulusHMR from 'vite-plugin-stimulus-hmr'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
+    StimulusHMR(),
   ],
 })
