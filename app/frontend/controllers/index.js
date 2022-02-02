8a9de6dae7c5ded8dd67945a8329371152fbac61
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-02 13:01:12 +0000

    Set up CableReady consumer

--- app/frontend/controllers/index.js
+++ app/frontend/controllers/index.js
@@ -6,6 +6,7 @@ import { registerControllers } from 'stimulus-vite-helpers'
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
 import controller from '../controllers/application_controller'
+import CableReady from 'cable_ready'
 
 const application = Application.start()
 const controllers = import.meta.globEager('./**/*_controller.js')
@@ -13,3 +14,4 @@ registerControllers(application, controllers)
 application.consumer = consumer
 StimulusReflex.initialize(application, { controller, isolate: true })
 StimulusReflex.debug = import.meta.env.DEV
+CableReady.initialize({ consumer })
