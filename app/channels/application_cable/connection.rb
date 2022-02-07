3dcca0797d223637da8740632918e0450925dc9d
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-07 10:38:50 +0000

    Set up ApplicationCable

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,9 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :request_id
+
+    def connect
+      self.request_id = Digest::SHA2.hexdigest(request.session.id.to_s)
+    end
   end
 end
