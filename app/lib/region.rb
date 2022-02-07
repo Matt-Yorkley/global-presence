9b439f58abfb96cb4178ac9e79fdae4c0a9e0b2a
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-02-07 10:21:22 +0000

    Add Region PORO

---
+++ app/lib/region.rb
@@ -0,0 +1,41 @@
+class Region
+  REGION_NAMES = {
+    dev: "Localhost, Nowhere",
+    ams: "Amsterdam, Netherlands",
+    cdg: "Paris, France",
+    fra: "Frankfurt, Germany",
+    hkg: "Hong Kong",
+    lhr: "London, United Kingdom",
+    maa: "Chennai (Madras), India",
+    nrt: "Tokyo, Japan",
+    scl: "Santiago, Chile",
+    sin: "Singapore",
+    syd: "Sydney, Australia",
+    yyz: "Toronto, Canada",
+    dfw: "Dallas, Texas (US)",
+    sjc: "Sunnyvale, California (US)",
+    sea: "Seattle, Washington (US)",
+    ord: "Chicago, Illinois (US)",
+    iad: "Ashburn, Virginia (US)",
+    lax: "Los Angeles, California (US)",
+    ewr: "Secaucus, NJ (US)",
+    gru: "São Paulo",
+    mad: "Madrid, Spain",
+    mia: "Miami, Florida (US)",
+    vin: "Virginia (US)"
+  }
+
+  class << self
+    def current
+      ENV.fetch("FLY_REGION", "dev")
+    end
+
+    def deployed_regions
+      ENV.fetch("DEPLOYED_REGIONS", "dev,dev,dev,dev,dev,dev").split(",")
+    end
+
+    def human_name(region_id = nil)
+      REGION_NAMES[(region_id || current).to_sym] || "Not found!? That's weird..."
+    end
+  end
+end
